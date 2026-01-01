54d0064d80102d4654cbde6fdd67a9c8657f7419
atharvamhaske <atharvamhaske76@gmail.com>
2026-01-01 15:39:04 +0000

    Code challenge submission for question: 217 Contains Duplicate

---
+++ 217 Contains Duplicate/217contains-duplicate.go
@@ -0,0 +1,16 @@
+func containsDuplicate(nums []int) bool {
+    if len(nums) <= 1 {
+        return false
+    }
+
+    sort.Ints(nums)
+
+    for i := 0; i < len(nums) - 1; i++ {
+        if nums[i] == nums[i+1] {
+            return true
+        }
+    }
+    return false
+}
+
+//good (used sorting and then comparing adjacent pairs)

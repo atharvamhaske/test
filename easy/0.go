247745d19e9862695df050675904403e92c32243
Atharva Mhaske <143622968+atharvamhaske@users.noreply.github.com>
2026-01-02 13:07:28 +0530

    Add 0 (Easy) - go

--- easy/0.go
+++ easy/0.go
@@ -1,12 +1,9 @@
 func twoSum(nums []int, target int) []int {
-    numMap := make(map[int]int) // A mapping to store numbers and their 
-indices
+    numMap := make(map[int]int) // A mapping to store numbers and their indices
     for i, num := range nums {
-        complement := target - num // Find the required number to reach 
-the target
+        complement := target - num // Find the required number to reach the target
         if index, found := numMap[complement]; found {
-            return []int{index, i} // Return indices of the complement and 
-current number
+            return []int{index, i} // Return indices of the complement and current number
         }
         numMap[num] = i // Store the number with its index
     }

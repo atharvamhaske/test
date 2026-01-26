f9ba4bf792ad54e91ef0770aa76bfb4472c3d31f
Atharva Mhaske <143622968+atharvamhaske@users.noreply.github.com>
2026-01-26 09:59:18 +0530

    Time: 0 ms (100%), Space: 5 MB (82.28%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
@@ -1,7 +1,26 @@
 func search(nums []int, target int) bool {
-    for _, v := range nums {
-        if v == target {
+    n := len(nums)
+    l, r := 0, n-1
+
+    for l <= r {
+        mid := l + (r-l)/2
+        if nums[mid] == target {
             return true
+        } else if nums[l] == nums[mid] && nums[mid] == nums[r] {
+            l += 1
+            r -= 1
+        } else if nums[l] <= nums[mid] {
+            if nums[l] <= target && target < nums[mid] {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[r] {
+                l = mid + 1
+            } else {
+                r =  mid - 1
+            }
         }
     }
     return false
